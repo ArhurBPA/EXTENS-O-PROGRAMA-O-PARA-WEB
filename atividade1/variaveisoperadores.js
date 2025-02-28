24f92a0d5e5f03fb325f98c120aee49b03979893
ArhurBPA <arthurbpa2005@gmail.com>
2025-02-28 10:51:29 -0300

    commit

---
+++ atividade1/variaveisoperadores.js
@@ -0,0 +1,23 @@
+let num_A = 10;
+let num_B = 20;
+let soma = num_A + num_B;
+
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é ${soma}`);
+
+let num_C = 15;
+let num_D = 25;
+let subtração = num_C - num_D;
+
+console.log(`Resultado da subtração entre ${num_C} e ${num_D} é ${subtração}`);
+
+let num_E = 20;
+let num_F = 30;
+let multiplicação = num_E * num_F;
+
+console.log(`Resultado da multiplicação entre ${num_E} e ${num_F} é ${multiplicação}`);
+
+let num_G = 25;
+let num_H = 35;
+let divisão = num_G / num_H;
+
+console.log(`Resultado da divisão entre ${num_G} e ${num_H} é ${divisão}`);
---
+++ variaveis_tioos/variaveis_tipos.js
@@ -0,0 +1,24 @@
+console.log('Hello Word');
+
+const n = 1
+console.log('n ='+n);
+
+var j = 3;
+var j = 4;
+console.log('j= ', j);
+
+let nome = 'Arthur';
+let telefone = '123456789';
+console.log(`Meu nome é ${nome}, meu telefone é ${telefone}`);
+
+let x = 3.14;
+x = 0.5;
+x = 1;
+
+let b = false;
+b = true;
+console.log(`b =  ${b}`);
+
+let c = 0;
+
+console.log(`${x} + ${x} = $(x + x)`);
